4143e6fd0ba0791ed86f08bba653f889749ba905
Dani Lyn <dlyn1110@gmail.com>
2019-02-10 18:53:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function BoardMember(name, homeState, training){
 
   BoardMember.prototype.veto = function(){
     return 'No, I must disagree'
-  };
 }
   BoardMember.prototype.approve = function(){
     return 'You can do that!'
